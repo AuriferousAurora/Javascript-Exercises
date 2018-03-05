4be494b7b65fe4b9693bb6ebd296b862e61a3541
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2018-03-05 15:03:59 -0600

    Renamed constructor property to fit with blackjack game.

--- Wk5-Day1/Objects/card.js
+++ Wk5-Day1/Objects/card.js
@@ -1,16 +1,15 @@
-function Card (point, suit, url) {
-    this.point = point;
+function Card (name, value, suit, img) {
+    this.name = name;
+    this.value = value;
     this.suit = suit;
-    this.url = url;
+    this.img = img;
 }
 
 var myArray = [];
 
 function Hand (card) {
     
-}
-
-
+};
 
 Hand.prototype.addCard = function(card) {
     myArray.push(card);
@@ -20,25 +19,40 @@ Hand.prototype.getPoints = function() {
     var sum = 0;
     for (i = 0; i <= myArray.length -1; i++) {
         console.log("debug");
-        sum += myArray[i].point;
+        sum += myArray[i].value;
         
         console.log(myArray[i]);
     };
     return sum;
 };
 
-var myCard = new Card(5, "diamonds", "images/5_of_diamonds.png")
+function Deck () {
+    var number = Math.floor(Math.random()*(myArray.length - 1));
+    return myArray[number];
+    myArray.splice(myArray[number], 1);
 
-console.log(myCard.point);
-console.log(myCard.suit);
-
-Card.prototype.getImageUrl = function() {
-    return this.url; 
 };
 
-console.log(myCard.getImageUrl());
+var myDeck = new Deck();
 
+Deck.prototype.shuffle = function(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+};
+};
+
+
+Card.prototype.getImageUrl = function() {
+    return this.img; 
+};
+
+var FiveD = new Card(5, "diamonds", "images/5_of_diamonds.png")
 var myHand = new Hand();
-myHand.addCard(myCard);
+myHand.addCard(FiveD);
 myHand.addCard(new Card(13, "spades", "images/ace_of_spades.png"));
+
 console.log(myHand.getPoints());
