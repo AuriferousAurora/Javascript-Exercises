5cd40cd59a6b88db1a8ed3f2edf5a93225e65a39
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2018-03-05 13:12:29 -0600

    Added hand constructor as well as addCard and getPoints methods.

--- Wk5-Day1/Objects/card.js
+++ Wk5-Day1/Objects/card.js
@@ -1,9 +1,44 @@
-function Card (point, suit) {
+function Card (point, suit, url) {
     this.point = point;
     this.suit = suit;
+    this.url = url;
 }
 
-var myCard = new Card(5, "diamonds")
+var myArray = [];
+
+function Hand (card) {
+    
+}
+
+
+
+Hand.prototype.addCard = function(card) {
+    myArray.push(card);
+};
+
+Hand.prototype.getPoints = function() {
+    var sum = 0;
+    for (i = 0; i <= myArray.length -1; i++) {
+        console.log("debug");
+        sum += myArray[i].point;
+        
+        console.log(myArray[i]);
+    };
+    return sum;
+};
+
+var myCard = new Card(5, "diamonds", "images/5_of_diamonds.png")
 
 console.log(myCard.point);
 console.log(myCard.suit);
+
+Card.prototype.getImageUrl = function() {
+    return this.url; 
+};
+
+console.log(myCard.getImageUrl());
+
+var myHand = new Hand();
+myHand.addCard(myCard);
+myHand.addCard(new Card(13, "spades", "images/ace_of_spades.png"));
+console.log(myHand.getPoints());
