5b6872c7b07e55b8d30e6d9ea49af3a0e8c54482
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2018-05-02 13:41:41 -0500

    Finished letter changes.

--- Wk13-Day1/letterChanges.js
+++ Wk13-Day1/letterChanges.js
@@ -1,41 +1,10 @@
-function letterChanges (input) {
-    var string = input.split();
-    var alphabet = /[a-z]/;
-    var vowels = /[aeiou]/;
-    betaString = [];
-    finalString = [];
-    // Intake a a string. Run a loop on each index in the string. If the index contains an alphabetical character, shift it one index right along a typical alphabet array.
-    string.forEach((iterator) => {
-        if (alphabet.test(iterator) === true) {
-            let newLetter = ((iterator.charCodeAt(0)) + 1);
-            if (newLetter === 123) {
-                let newLetter = 97;
-
-            } else {
-                stringLetter = newLetter.toString();
-                betaString.push(stringLetter.fromCharCode(0));
-            }
-        } else {
-            betaString.push(iterator);
-        }
+function letterChanges (str) {
+    var converted = str.replace(/[a-z]/gi, function(iterator) {
+        return (iterator === 'z' || iterator === 'Z') ? 'a' : String.fromCharCode(iterator.charCodeAt() + 1);
+    });
+    var capitalized = converted.replace(/a|e|i|o|u/gi, function(iterator) {
+        return iterator.toUpperCase();
     });
 
-    // betaString.forEach ((iterator) => {
-    //     if (vowels.test(iterator) === true) {
-    //         let captial = iterator.toUpperCase();
-    //         finalString.push(captial);
-    //     } else {
-    //         finalString.push(iterator);
-    //     }
-    // });
-
-    console.log(betaString);
-    return finalString;
-
-
-    // For any non-alphabateical character, return the character unmutated.
-
-    // If any new character is a vowel, capitalize it.
+    return capitalized;
 }
-
-letterChanges('hello*3');
